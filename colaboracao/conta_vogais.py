8685357206b49e86370bbff0c387bd581c67aeb2
Gabriel Cardoso <bielsonaro64@gmail.com>
2026-04-23 14:25:54 -0300

    Add vowel counting script in conta_vogais.py

---
+++ colaboracao/conta_vogais.py
@@ -0,0 +1,6 @@
+# Desafio 8: Contador de Vogais
+frase = input("Digite uma frase: ").lower()
+vogais = "aeiou"
+contador = sum(1 for letra in frase if letra in vogais)
+
+print(f"A frase contém {contador} vogais.")
